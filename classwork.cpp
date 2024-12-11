f0d2024038521d09e372905f7a8d19f1bd6ee0f3
VOider8956 <99462247+VOider8956@users.noreply.github.com>
2024-12-11 21:04:20 +0000

    I learnt alot

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,12 @@
 
 int main()
 {
-  // write code here
+  int age = 10; //SET age TO 10
+  double price = 10.99;
+  bool isRaining = false;
   
+  std::cout << age << "\n " << price << "\n" << isRaining << std::endl; //cout allows to
+  int grade_level = 12;
+  std::cout << "I am a " << grade_level << "th grader." << std::endl;
   return 0;
 }
